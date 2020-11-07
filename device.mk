a0839655bb4a7e1ed31330ed699f7b8836025138
Magicxavi <rolfpeterkesche@gmail.com>
2020-11-07 20:11:08 +0200

    kenzo: enable iwlan

--- device.mk
+++ device.mk
@@ -579,3 +579,8 @@ PRODUCT_ODM_PROPERTIES += \
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
+
+# IWlan  
+PRODUCT_PROPERTY_OVERRIDES += \
+     persist.vendor.data.iwlan.enable=true \
+     ro.telephony.iwlan_operation_mode=legacy
